b9681964c63859ce626113660b2738209d1958ea
Zenoson <zenoson1234@gmail.com>
2022-05-24 12:15:51 -0400

    added basic laser outline thingy

---
+++ laserObjects.ts
@@ -0,0 +1,29 @@
+class Laser {
+    
+    //List all properties:
+    m : number; // m: [0,1)
+    b : number;
+    type : number;
+
+    /*
+    type 1: y = mx+b
+    type 2: y = -mx+b
+    type 3: x = my+b
+    type 4: x = -my+b
+    */
+
+    constructor(m : number, b : number, type : number) {
+        //set up properties
+        this.m = m;
+        this.b = b;
+        this.type = type
+    }
+
+    draw() : void {
+
+    }
+
+    update() : void {
+        
+    }
+}
